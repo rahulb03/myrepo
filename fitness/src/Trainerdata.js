9ea9268d4e52ae550243db1761acbd7617b37d39
rahulb03 <122718029+rahulb03@users.noreply.github.com>
2024-05-08 17:22:07 +0530

    Add files via upload

---
+++ fitness/src/Trainerdata.js
@@ -0,0 +1,74 @@
+import imageArray from "./imagearray";
+const trainerData = [
+    {
+      id: 1,
+      name: 'John Doe',
+      rating: 4.5,
+      image: imageArray[10],
+      about: "The report of the excavation concluded that there were ruins of a massive structure beneath the ruins of the mosque which was indicative of remains which are distinctive features found associated with the temples of north India, but did not conclusively conclude that the structure was a temple, nor did the survey .",
+
+      location:"surat"
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      rating: 4.8,
+      image: imageArray[11],
+      about: "The report of the excavation concluded that there were ruins of a massive structure beneath the ruins of the mosque which was indicative of remains which are distinctive features found associated with the temples of north India, but did not conclusively conclude that the structure was a temple, nor did the survey .",
+      location:"surat"
+
+    },
+    {
+      id: 1,
+      name: 'John Doe',
+      rating: 4.5,
+      image: imageArray[10],
+      about: "The report of the excavation concluded that there were ruins of a massive structure beneath the ruins of the mosque which was indicative of remains which are distinctive features found associated with the temples of north India, but did not conclusively conclude that the structure was a temple, nor did the survey .",
+      location:"surat"
+
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      rating: 4.8,
+      image: imageArray[11],
+      about: "The report of the excavation concluded that there were ruins of a massive structure beneath the ruins of the mosque which was indicative of remains which are distinctive features found associated with the temples of north India, but did not conclusively conclude that the structure was a temple, nor did the survey .",
+      location:"surat"
+
+     
+    },
+   
+    {
+      id: 2,
+      name: 'Jane Smith',
+      rating: 4.8,
+      image: imageArray[11],
+      about: "The report of the excavation concluded that there were ruins of a massive structure beneath the ruins of the mosque which was indicative of remains which are distinctive features found associated with the temples of north India, but did not conclusively conclude that the structure was a temple, nor did the survey .",
+      location:"surat"
+
+    },
+    {
+      id: 1,
+      name: 'John Doe',
+      rating: 4.5,
+      image: imageArray[10],
+      about: "The report of the excavation concluded that there were ruins of a massive structure beneath the ruins of the mosque which was indicative of remains which are distinctive features found associated with the temples of north India, but did not conclusively conclude that the structure was a temple, nor did the survey .",
+      location:"surat"
+
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      rating: 4.8,
+      image: imageArray[11],
+      about: "The report of the excavation concluded that there were ruins of a massive structure beneath the ruins of the mosque which was indicative of remains which are distinctive features found associated with the temples of north India, but did not conclusively conclude that the structure was a temple, nor did the survey .",
+      location:"surat"
+
+     
+    },
+    
+   
+   
+  ];
+
+  export default trainerData;
